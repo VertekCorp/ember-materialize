25e1285df179f4a87a5170a003e0b64975ddfdac
Ryan Connolly <rynam0@gmail.com>
2017-03-03 08:53:31 -0500

    remove invalid test, checkboxes are not block helpers

--- tests/integration/components/em-checkbox-test.js
+++ tests/integration/components/em-checkbox-test.js
@@ -6,20 +6,8 @@ moduleForComponent('em-checkbox', 'Integration | Component | em checkbox', {
 });
 
 test('it renders', function(assert) {
-
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{em-checkbox}}`);
-
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#em-checkbox}}
-      template block text
-    {{/em-checkbox}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });

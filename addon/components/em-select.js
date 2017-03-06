71fe62013fbe1029bfc9d4e6c78b72dd4ea195c1
Ryan Connolly <rynam0@gmail.com>
2017-03-06 13:19:47 -0500

    allow selection of objects in em-select

--- addon/components/em-select.js
+++ addon/components/em-select.js
@@ -2,22 +2,28 @@ import Ember from 'ember';
 import layout from '../templates/components/em-select';
 
 export default Ember.Component.extend({
-    layout,
-    classNames: ['input-field'],
-    prompt: 'Select...',
-    didInsertElement() {
-        this._super(...arguments);
+  layout,
+  classNames: ['input-field'],
+  prompt: 'Select...',
+  didInsertElement() {
+    this._super(...arguments);
 
-        let value = this.get('value');
-        Ember.$.each(this.$('option'), function(name, option) {
-          if (option.value === value) {
-            Ember.$(option).attr('selected', 'selected');
-          }
-        });
-        this.$('select').material_select();
-    },
-    willDestroyElement() {
-        this._super(...arguments);
-        this.$('select').material_select('destroy');
+    let value = this.get('value');
+    Ember.$.each(this.$('option'), function(name, option) {
+      if (option.value === value) {
+        Ember.$(option).attr('selected', 'selected');
+      }
+    });
+    this.$('select').material_select();
+  },
+  willDestroyElement() {
+    this._super(...arguments);
+    this.$('select').material_select('destroy');
+  },
+  actions: {
+    'on-change'(e) {
+      this.set('value', e);
+      this.set('selection', this.get('content').findBy(this.get('optionValuePath'), e));
     }
+  }
 });

8912a9d984e15624984668226d43281623dd5fa5
Ryan Connolly <rynam0@gmail.com>
2017-03-07 11:02:15 -0500

    listen for focusOut on inputs

--- addon/components/em-input.js
+++ addon/components/em-input.js
@@ -3,18 +3,28 @@ import layout from '../templates/components/em-input';
 const { Component, isPresent, computed } = Ember;
 
 export default Component.extend({
-    layout,
-    type: 'text',
-    disabled: false,
-    classNames: ['input-field'],
-    id: computed('elementId', function() {
-        return `input-${this.get('elementId')}`;
-    }),
-    didInsertElement() {
-        this._super(...arguments);
-        const $label = this.$('> label');
-        if (isPresent(this.get('value')) && !$label.hasClass('active')) {
-            $label.addClass('active');
-        }
+  layout,
+  type: 'text',
+  disabled: false,
+  classNames: ['input-field'],
+  _wasBlurred: false,
+  id: computed('elementId', function() {
+    return `input-${this.get('elementId')}`;
+  }),
+  didInsertElement() {
+    this._super(...arguments);
+    const $label = this.$('> label');
+    if (isPresent(this.get('value')) && !$label.hasClass('active')) {
+        $label.addClass('active');
     }
+  },
+
+  actions: {
+    'on-focus-out'() {
+      console.log('heard focus out :)');
+      if (!this.get('_wasBlurred')) {
+        this.set('_wasBlurred', true);
+      }
+    }
+  }
 });

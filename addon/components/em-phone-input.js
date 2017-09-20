7e96d6a3ddcf570525b253bf65230be9d294a8f5
franktcurran <franktcurran@gmail.com>
2017-09-20 14:54:14 -0400

    Provide a phone number input with appropriate masking.

--- addon/components/em-phone-input.js
+++ addon/components/em-phone-input.js
@@ -1,7 +1,5 @@
-import Ember from 'ember';
 import EmInput from './em-input';
 import layout from '../templates/components/em-phone-input';
-const { Component, isPresent, computed, defineProperty } = Ember;
 
 export default EmInput.extend({
   layout
